93fa32dd2ccd50e5ee5855c55ff9541998df5e00
Darshil Sharma <darshil.ips@gmail.com>
2026-07-29 13:41:41 +0530

    The new projeect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from firebase_config import db
 
 app = Flask(__name__,template_folder="templates")
 app.config['SECRET_KEY'] = 'Secret_key'
@@ -8,29 +7,12 @@ app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'Secret-key'
 
 @app.route('/')
-def landing_page():
-    return render_template("landing_page.html")
+def about_page():
+    return render_template("abut.html")
 
-@app.route('/Prof')
-def teacher_page():
-    return render_template("prof_page.html")
-
-@app.route('/login/prof')
-def teacher_login():
-    return render_template("login_page_teacher.html")
-
-@app.route('/login/student')
-def student_login():
-    return render_template("login_page_student.html")
-
-@app.route('/login/student/dashboard')
-def student_dashboard():
-    return render_template("dashboard.html")
-
-@app.route('/login/teacher/dashboard')
-def teacher_dashboard():
-    return render_template("teacher_dashboard.html")
-
+@app.route('/terms')
+def terms_condition():
+    return render_template('terms.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
